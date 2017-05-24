df07c7a0093ab7b88f40d2a8af3a8c2f2578e947
Joseph Bolen <bolenonline@roadrunner.com>
2017-05-24 19:14:55 -0400

    Initial Hello World  app in Classic Windows Forms.

--- WindowsHelloWorld/MainForm.vb
+++ WindowsHelloWorld/MainForm.vb
@@ -3,6 +3,6 @@
     Private Sub ClickMeButton_Click(sender As Object, e As EventArgs) _
         Handles ClickMeButton.Click
 
-        ResultLabel.Text = "Hello World!"
+        ResultLabel.Text = "Hello World!" 'my comment
     End Sub
 End Class
